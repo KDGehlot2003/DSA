f60400a7b9d973476696b9b78f1089602216ce44
KDGehlot2003 <kdgehlot2003@gmail.com>
2023-12-26 20:21:17 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,22 @@
+# from array import *
+
+# my_arr = array('i',[1,2,3,4,5])
+# print(my_arr)
+
+# for i in my_arr:
+#     print(i)
+
+# print(my_arr[3])
+
+# my_arr.append(6)
+# print(my_arr)
+
+# my_arr.insert(2,11)
+# print(my_arr)
+
+# my_arr1 = array('i', [10,11,12])
+# my_arr.extend(my_arr1)
+# print(my_arr)
+
+import numpy as np
+
